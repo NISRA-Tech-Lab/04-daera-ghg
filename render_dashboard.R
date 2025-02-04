16d5930ac113f24dbd245d21eb9a403e7f42b625
brianq-nisra <brian.quinn@nisra.gov.uk>
2025-02-04 10:05:06 +0000

    move to docs folder

--- render_dashboard.R
+++ render_dashboard.R
@@ -7,11 +7,11 @@ quarto_render(input = paste0(here(), "/pages"),
               as_job = FALSE)
 
 # Run after render to fix date slider
-html_docs <- list.files(paste0(here(), "/_site"), pattern = "*.html")
+html_docs <- list.files(paste0(here(), "/docs"), pattern = "*.html")
 
 for (doc in html_docs) {
   
-  raw_html <- readLines(paste0(here(), "/_site/", doc))
+  raw_html <- readLines(paste0(here(), "/docs/", doc))
   
   if (length(which(grepl("strftime", raw_html))) > 0) {
     strftime_line <- which(grepl("strftime", raw_html))
@@ -23,11 +23,11 @@ for (doc in html_docs) {
                   raw_html[(strftime_line + 2):length(raw_html)])
     
     writeLines(new_html,
-               paste0(here(), "/_site/", doc))
+               paste0(here(), "/docs/", doc))
     
   }
   
 }
 
 # After render is complete run this to see output in browser
-browseURL(paste0(here(), "/_site/index.html"))
+browseURL(paste0(here(), "/docs/index.html"))
